2da7e4ec0c2ab5585e1dec7e5a904cd7b6c708f7
Niko Granö <Niko9911@users.noreply.github.com>
2019-10-16 20:29:39 +0300

    Update 20-migrations.sh

--- 20-migrations.sh
+++ 20-migrations.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-PGDATA='/var/lib/postgresql/data'
-export PGDATA="$PGDATA"
-
-grep -qxF '127.0.0.1 db' /etc/hosts || echo '127.0.0.1 db' >> /etc/hosts
-
 cd /var/www/html
 PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
 if [ "$APP_ENV" != 'prod' ]; then
@@ -15,7 +10,6 @@ ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
 
 mkdir -p var/cache var/log
 
-gosu postgres pg_ctl -D "$PGDATA" -m fast -w start
 echo "Waiting for db to be ready..."
 REPEAT=0
 until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
@@ -23,7 +17,7 @@ until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
   REPEAT=$((REPEAT + 1))
   sleep 1
 
-  if [ $REPEAT -eq 15 ]; then
+  if [ $REPEAT -eq 30 ]; then
     echo "Database connection failed!"
     exit 1;
   fi
@@ -32,5 +26,3 @@ done
 if ls -A src/Migrations/*.php > /dev/null 2>&1; then
   bin/console doctrine:migrations:migrate --no-interaction
 fi
-
-gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
