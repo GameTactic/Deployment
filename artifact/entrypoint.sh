4f197591c9ab3af21dc8b495b7977b33a274cd53
Niko Granö <niko@ironlions.fi>
2020-02-25 16:02:57 +0200

    Update entrypoint.sh

--- artifact/entrypoint.sh
+++ artifact/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-if [[ -z "${GT_MIGRATE}" ]]; then
+if [ -f /app_migrate ]; then
+  echo "Waiting for db to be ready..."
+  REPEAT=0
+  until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
+    echo -ne '.'
+    REPEAT=$((REPEAT + 1))
+    sleep 1
+
+    if [ $REPEAT -eq 30 ]; then
+      echo "Database connection failed!"
+      exit 1;
+    fi
+  done
   echo 'Running migrations...'
   /usr/local/bin/php /app/bin/console doctrine:migrations:migrate latest --no-interaction
 fi
