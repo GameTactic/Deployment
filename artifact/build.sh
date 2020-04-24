28195087dafa7323df7e2ee593f4f1b1c35c5820
Niko Granö <niko@ironlions.fi>
2020-04-24 03:05:25 +0300

    Update build.sh

--- artifact/build.sh
+++ artifact/build.sh
@@ -9,6 +9,13 @@ curl -Ss https://raw.githubusercontent.com/GameTactic/Deployment/master/artifact
 curl -Ss https://raw.githubusercontent.com/GameTactic/Deployment/master/artifact/docker.conf > /usr/local/etc/php-fpm.d/docker.conf
 echo -e "[PHP]\nupload_max_filesize = 2M\npost_max_size = 4M\n" > /usr/local/etc/php/php.ini
 
+# Install Nginx Metrics Export
+wget https://github.com/nginxinc/nginx-prometheus-exporter/releases/download/v0.7.0/nginx-prometheus-exporter-0.7.0-linux-amd64.tar.gz
+tar zxvf nginx-prometheus-exporter-0.7.0-linux-amd64.tar.gz
+rm nginx-prometheus-exporter-0.7.0-linux-amd64.tar.gz
+mv nginx-prometheus-exporter /bin
+
+# Configure container runtime
 curl -Ss https://raw.githubusercontent.com/GameTactic/Deployment/master/artifact/supervisord.conf > /etc/supervisord.conf
 curl -Ss https://raw.githubusercontent.com/GameTactic/Deployment/master/artifact/entrypoint.sh > /entrypoint.sh
 chmod +x /entrypoint.sh
