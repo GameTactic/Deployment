db80ea5bcc3beefea6526709c72335c44fd661c7
Niko Granö <niko@gametactic.eu>
2021-03-31 22:47:37 +0300

    Fix permissions being wrong

--- artifact/entrypoint.sh
+++ artifact/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ -f /app_migrate ]; then
   echo "Waiting for db to be ready..."
   REPEAT=0
-  until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
+  until sudo -E -u www-data bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
     echo -ne '.'
     REPEAT=$((REPEAT + 1))
     sleep 1
@@ -14,7 +14,7 @@ if [ -f /app_migrate ]; then
     fi
   done
   echo 'Running migrations...'
-  /usr/local/bin/php /app/bin/console doctrine:migrations:migrate latest --no-interaction
+  sudo -E -u www-data bin/console doctrine:migrations:migrate latest --no-interaction
 fi
 
 echo 'Starting Supervisor...'
